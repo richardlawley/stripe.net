9c47c474a6b2e5d802a693c50e4c84b832e3090f
Richard Lawley <richard@richardlawley.com>
2014-01-12 19:19:40 +0000

    Add Expand Properties for Options

---
+++ src/Stripe/Services/ApplicationFees/StripeApplicationFeeGetOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Stripe
+{
+	public class StripeApplicationFeeGetOptions 
+	{
+		public bool ExpandAccount { get; set; }
+		public bool ExpandBalance_Transaction { get; set; }
+	}
+}
---
+++ src/Stripe/Services/InvoiceItems/StripeInvoiceItemListOptions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Stripe
+{
+	public class StripeInvoiceItemListOptions : StripeListOptions
+	{
+		[JsonProperty("customer")]
+		public string CustomerId { get; set; }
+
+		[JsonProperty("created")]
+		public StripeDateFilter Created { get; set; }
+
+		public bool ExpandCustomer { get; set; }
+		public bool ExpandInvoice { get; set; }
+	}
+}
